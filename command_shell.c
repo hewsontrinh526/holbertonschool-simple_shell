4090153da14643c933efc475eedee77abea6e854
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 05:08:21 -0700

    Updating command shell

--- command_shell.c
+++ command_shell.c
@@ -16,3 +16,48 @@ void get_input(char **buffer, size_t *bufsize, ssize_t *read)
 		exit(EXIT_SUCCESS);
 	}
 }
+
+int fork_the_child(char **command, char **environ, char **str)
+{
+	pid_t child;
+	char *filepath;
+	int status;
+
+	child = fork();
+
+	if (child == 0)
+	{
+		if (strchr(command[0], '/') != NULL)
+		{
+			if (execve(command[0], command, environ) == -1)
+			{
+				perror("Error executing command");
+				free(str);
+				exit(127);
+			}
+		}
+		else
+		{
+			filepath = find_executable_in_path(command[0]);
+			if (filepath != NULL)
+			{
+				if (execve(filepath, command, environ) == -1)
+				{
+					perror("Error executing command");
+					free(str);
+					exit(127);
+				}
+			}
+			else
+			{
+				printf("%s: command not found\n", command[0]);
+				return (-1);
+			}
+		}
+	}
+	if (child > 0)
+	{
+		wait(&status);
+	}
+	return (status);
+}

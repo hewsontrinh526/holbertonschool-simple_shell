8bf39caf9802401b3ae6d03fd9893139f59da930
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 06:37:11 -0700

    Updating shell

--- shell.c
+++ shell.c
@@ -5,9 +5,9 @@ int main(void)
 	char *buffer;
 	size_t bufsize;
 	ssize_t read;
-	char *str;
 	int exit_status;
         char *command[100];
+	char *str;
 
         while (1)
         {
@@ -22,7 +22,7 @@ int main(void)
 		line_to_array(str, command);
 
 		exit_status = fork_the_child(command, environ, &str);
+		free(str);
 	}
-	free(str);
 	exit(exit_status);
 }

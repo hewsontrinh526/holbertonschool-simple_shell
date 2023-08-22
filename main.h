c0176229736c1973d3b6bc0cb18198de0929fd04
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 00:52:24 -0700

    Updating main

--- main.h
+++ main.h
@@ -1,15 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-typedef struct s_dir
-{
-	char *directory;
-	struct s_dir *next;
-} t_dir;
-
-t_dir *create_node(char *dir);
-void add_node_end(t_dir **head, char *dir);
-t_dir *build_path_list(char *path);
-void free_list(t_dir *head);
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
 
 #endif

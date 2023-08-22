007297d3206349fb999fcd8bec3acdc6acfcffcf
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 05:36:59 -0700

    Updating shell

--- shell.c
+++ shell.c
@@ -7,9 +7,7 @@ int main(void)
 	ssize_t read;
 	char *str;
 	int exit_status;
-        char *command[16];
-	char *tok;
-	size_t i;
+        char *command[100];
 
         while (1)
         {
@@ -19,20 +17,9 @@ int main(void)
 		get_input(&buffer, &bufsize, &read);
 
 		str = strdup(buffer);
-
 		free(buffer);
 
-		tok = strtok(str, " \t\n\r");
-                i = 0;
-
-		while (i < 16 && tok != NULL)
-                {
-                        command[i] = tok;
-                        tok = strtok(NULL, " \t\n\r");
-                        i = i + 1;
-                }
-
-		command[i] = NULL;
+		line_to_array(str, command);
 
 		exit_status = fork_the_child(command, environ, &str);
 	}

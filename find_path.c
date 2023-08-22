b8c1950d3783bc2ce41c01738fd66f6a44bfb537
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 02:36:55 -0700

    Updating find_path

---
+++ find_path.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+char *_getenv(const char *name)
+{
+	int i = 0;
+	int l = strlen(name); /* Length */
+
+	while(environ[i] != NULL)
+	{
+		if (strncmp(environ[i], name, l) == 0 && environ[i][l] == '=')
+		{
+			return(&environ[i][l + 1]);
+		}
+		i = i + 1;
+	}
+	return (NULL);
+}
+
+struct stat st;
+
+char *find_executable_in_path(char *cmd)
+{
+	char *path = strdup(_getenv("PATH"));
+	char *dir = strtok(path, ":");
+
+	while (dir != NULL)
+	{
+		char *filepath = malloc(strlen(dir) + strlen(cmd) + 2);
+		sprintf(filepath, "%s/%s", directory, cmd);
+
+		if (stat(filepath, &st) == 0 && st.st_mode & S_IXUSR)
+		{
+			return (filepath);
+		}
+		free(filepath);
+		dir = strtok(NULL, ":");
+	}
+	free(path);
+	return (NULL);
+}

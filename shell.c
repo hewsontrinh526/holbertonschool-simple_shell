a4ab0d2df313b8c5a0f48933faeeedd5193e2842
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 23:13:12 -0700

    Updating shell

--- shell.c
+++ shell.c
@@ -25,7 +25,7 @@ int main(void)
 
 		line_to_array(str, command);
 
-		built_in_checker = checkbuiltin(command, &status, str);
+		built_in_checker = checkbuiltin(command, environ, &status, str);
 
 		if (built_in_checker == 1)
 		{

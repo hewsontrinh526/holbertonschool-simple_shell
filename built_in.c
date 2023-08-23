5b1958fcc83522797aa4278c0b81cf8624f2829b
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-23 00:06:24 -0700

    Updating built in

--- built_in.c
+++ built_in.c
@@ -15,7 +15,7 @@ int print_env(char **array)
 }
 
 
-int checkbuiltin(char **array, char **environ, int *status, char *str)
+int checkbuiltin(char **array, char **environ, char *str)
 {
 	if (array[0] == NULL)
 	{
@@ -27,7 +27,7 @@ int checkbuiltin(char **array, char **environ, int *status, char *str)
 	{
 		free(str);
 		free(array);
-		exit(2);
+		exit(EXIT_SUCCESS);
 	}
 	else if (strcmp(array[0], "env") == 0)
 	{

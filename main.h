f899ac14e15072bfb8ebb2f72c55c7b29ca9588e
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-22 21:37:26 -0700

    Updating main.h

--- main.h
+++ main.h
@@ -17,13 +17,17 @@ typedef struct builtin_cmd
         int (*f)(void);
 } builtin_cmd_t;
 
+extern char **environ;
+
 void get_input(char **buffer, size_t *bufsize, ssize_t *read);
 int fork_the_child(char **command, char **environ, char **str);
 void line_to_array(char *str, char **command);
 int execute_builtin_cmd(char *tokenised_array);
-int print_env();
-int exit_shell();
-extern char **environ;
+
+int print_env(void);
+int exit_shell(void);
+int check_built_in(char **array);
+
 char *_getenv(const char *name);
 char *find_executable_in_path(char *command);
 

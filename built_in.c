9abf34c04e200ea20f08b20741b285cdd3384240
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-24 03:32:03 -0700

    Final version of simple shell

--- built_in.c
+++ built_in.c
@@ -20,21 +20,45 @@ int checkbuiltin(char **array, char **environ, char *str, int *status)
 	if (array[0] == NULL)
 	{
 		free(str);
-		free(array);
 		return (1);
 	}
 	if (strcmp(array[0], "exit") == 0)
 	{
 		free(str);
-		free(array);
 		exit(*status);
 	}
 	else if (strcmp(array[0], "env") == 0)
 	{
 		print_env(environ);
 		free(str);
-		free(array);
 		return (1);
 	}
 	return (0);
+}
+
+DIR *_opendir(char *name)
+{
+	DIR *dp;
+
+	dp = opendir(name);
+
+	if (dp == NULL)
+	{
+		perror("open directory");
+		return (NULL);
+	}
+	return (dp);
+}
+
+struct dirent *_readdir(DIR *dp)
+{
+	struct dirent *ep;
+
+	ep = readdir(dp);
+
+	if (ep == NULL)
+	{
+		return (NULL);
+	}
+	return (ep);
 }

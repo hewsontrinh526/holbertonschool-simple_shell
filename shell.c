504a3df1c43f58439fb2c38f2a8fc5fcacbf1385
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-24 03:31:50 -0700

    Final version of simple shell

--- shell.c
+++ shell.c
@@ -1,40 +1,56 @@
 #include "main.h"
 
-int main(void)
+int main(__attribute__((unused)) int ac, char *av[])
 {
 	char *buffer;
 	size_t bufsize;
 	ssize_t read;
-        char **command;
+        char *cmd[124];
 	char *str;
 	int built_in_checker;
 	int exit_status;
 	int status;
+	int k;
+
+	status = 0;
 
 	while (1)
         {
 		buffer = NULL;
 		bufsize = 0;
+		k = 0;
 
 		get_input(&buffer, &bufsize, &read);
 
 		str = strdup(buffer);
 		free(buffer);
 
-		command = malloc(2048);
+		line_to_array(str, cmd);
 
-		line_to_array(str, command);
-
-		built_in_checker = checkbuiltin(command, environ, str, &status);
+		built_in_checker = checkbuiltin(cmd, environ, str, &status);
 
 		if (built_in_checker == 1)
 		{
 			continue;
 		}
+		if (executable_cmd(cmd[0]) == 0)
+		{
+			cmd[0] = find_executable_in_path(&(cmd[0]), av[0]);
+			k = 1;
 
-		exit_status = fork_the_child(command, environ, &str);
+			if (cmd[0] == NULL)
+			{
+				free(str);
+				return (127);
+			}
+		}
 
-		free(command);
+		exit_status = fork_the_child(cmd, environ, &str);
+
+		if (k == 1)
+		{
+			free(cmd[0]);
+		}
 		free(str);
 	}
 	exit(exit_status);

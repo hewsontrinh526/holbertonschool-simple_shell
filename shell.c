f909671d0c91b8a5854ece07a26a54c41d919ec5
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-24 16:07:13 -0700

    Added betty descriptions

--- shell.c
+++ shell.c
@@ -10,33 +10,23 @@
  */
 int main(__attribute__((unused)) int ac, char *av[])
 {
-	char *buffer;
 	size_t bufsize;
 	ssize_t read;
-        char *cmd[1024];
-	char *str;
-	int built_in_checker;
-	int exit_status;
-	int status;
-	int k;
+	char *cmd[1024], *str, *buffer;
+	int built_in_checker, exit_status, status, i;
 
 	status = 0;
 
 	while (1)
-        {
+	{
 		buffer = NULL;
 		bufsize = 0;
-		k = 0;
-
+		i = 0;
 		get_input(&buffer, &bufsize, &read);
-
 		str = strdup(buffer);
 		free(buffer);
-
 		line_to_array(str, cmd);
-
 		built_in_checker = checkbuiltin(cmd, environ, str, &status);
-
 		if (built_in_checker == 1)
 		{
 			continue;
@@ -44,18 +34,15 @@ int main(__attribute__((unused)) int ac, char *av[])
 		if (executable_cmd(cmd[0]) == 0)
 		{
 			cmd[0] = find_executable_in_path(&(cmd[0]), av[0]);
-			k = 1;
-
+			i = 1;
 			if (cmd[0] == NULL)
 			{
 				free(str);
 				return (127);
 			}
 		}
-
 		exit_status = fork_the_child(cmd, environ, &str, &status);
-
-		if (k == 1)
+		if (i == 1)
 		{
 			free(cmd[0]);
 		}

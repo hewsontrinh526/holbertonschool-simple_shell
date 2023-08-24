3bc2c819f3347a1b177cb062047aea151dc1374d
hewsontrinh526 <6734@holbertonstudents.com>
2023-08-24 03:32:08 -0700

    Final version of simple shell

--- find_path.c
+++ find_path.c
@@ -1,41 +1,104 @@
 #include "main.h"
 
-char *_getenv(const char *name)
+char *_getenv(char *name)
 {
-	int i = 0;
-	int l = strlen(name); /* Length */
+	int i;
+	int n;
+	char *string;
 
-	while(environ[i] != NULL)
+	i = 0;
+	n = strlen(name);
+
+	while (environ[i] != NULL)
 	{
-		if (strncmp(environ[i], name, l) == 0 && environ[i][l] == '=')
+		if (strncmp(environ[i], name, n) == 0)
 		{
-			return(&environ[i][l + 1]);
+			string = strchr(environ[i], '=');
+			return (string);
 		}
 		i = i + 1;
 	}
 	return (NULL);
 }
 
-struct stat st;
-
-char *find_executable_in_path(char *cmd)
+char *find_executable_in_path(char **cmd, char *prog_av)
 {
 	char *path = strdup(_getenv("PATH"));
-	char *dir = strtok(path, ":");
+	char *dir;
+	char *each_path[100];
+	int i;
+	DIR *dp;
+	struct dirent *ep;
 
-	while (dir != NULL)
+	path = strdup(_getenv("PATH="));
+
+	if (path == NULL)
 	{
-		char *filepath = malloc(strlen(dir) + strlen(cmd) + 2);
-		sprintf(filepath, "%s/%s", dir, cmd);
+		fprintf(stderr, "%s: %s: %s: not found\n", prog_av, "1", *cmd);
+		return (NULL);
+	}
 
-		if (stat(filepath, &st) == 0 && st.st_mode & S_IXUSR)
+	dir = strdup(path + 1);
+	string_into_words(dir, each_path);
+
+	i = 0;
+
+	while (each_path[i] != NULL && i < 99)
+	{
+		dp = _opendir(each_path[i]);
+		ep = _readdir(dp);
+
+		while (ep != NULL)
 		{
-			free(path);
-			return (filepath);
+			if (strcmp(*cmd, ep->d_name) == 0)
+			{
+				*cmd = get_exe_string(each_path[i], ep->d_name);
+				free(path);
+				closedir(dp);
+				return (*cmd);
+			}
+			ep = _readdir(dp);
 		}
-		free(filepath);
-		dir = strtok(NULL, ":");
+		closedir(dp);
+		i = i + 1;
 	}
 	free(path);
+	fprintf(stderr, "%s: %s: %s: not found\n", prog_av, "1", *cmd);
 	return (NULL);
+}
+
+void string_into_words(char *string, char **words)
+{
+	char *word;
+	int i;
+
+	i = 0;
+	word = strtok(string, ":");
+
+	while (word != NULL && i < 99)
+	{
+		words[i] = word;
+		i = i + 1;
+		word = strtok(NULL, ":");
+	}
+	words[i] = NULL;
+}
+
+char *get_exe_string(char *path, char *program_av)
+{
+	char *string;
+	size_t size;
+
+	size = sizeof(*string) * (strlen(program_av) + 2 + strlen(path));
+	string = malloc(size * 1);
+
+	if (string == NULL)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	strcat(string, path);
+	strcat(string, "/");
+	strcat(string, program_av);
+	return (string);
 }

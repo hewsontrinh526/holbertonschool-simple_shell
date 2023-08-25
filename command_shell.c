8e04a21488df8b0ecc124c953c84e6b90a4b05a5
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-24 20:52:06 -0700

    function descriptions for command_shell

--- command_shell.c
+++ command_shell.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * get_input - Reads a line of input from the standard input, displaying a prompt if the input is a terminal.
+ * @buffer: A pointer to the buffer where the input will be stored.
+ * @bufsize: A pointer to the size of the buffer.
+ * @read: A pointer to a variable that stores the number of characters read.
+ */
 void get_input(char **buffer, size_t *bufsize, ssize_t *read)
 {
 	signal(SIGINT, SIG_IGN);
@@ -17,7 +22,14 @@ void get_input(char **buffer, size_t *bufsize, ssize_t *read)
 		exit(EXIT_SUCCESS);
 	}
 }
-
+/**
+ * fork_the_child - Forks a child process to execute a given command using execve.
+ * @command:  An array of strings representing the command and its arguments.
+ * @environ: An array of strings representing the environment variables.
+ * @str: A pointer to a string that may be associated with the command and is freed if execve fails.
+ * @status: A pointer to an integer representing the child's exit status or termination signal.
+ * Return: The exit status or termination signal of the child process.
+ */
 int fork_the_child(char **command, char **environ, char **str, int *status)
 {
 	int child;
@@ -49,7 +61,11 @@ int fork_the_child(char **command, char **environ, char **str, int *status)
 	}
 	return (*status);
 }
-
+/**
+ * line_to_array - Tokenizes a command line string into an array of strings, splitting it by whitespace characters. 
+ * @str: A string representing the command line input.
+ * @command: A pointer to the array where the tokenized command arguments will be stored.
+ */
 void line_to_array(char *str, char **command)
 {
 	int i;
@@ -68,7 +84,11 @@ void line_to_array(char *str, char **command)
 	}
 	command[i] = NULL;
 }
-
+/**
+ * executable_cmd - Checks if a given file (represented by the string) is executable by the user.
+ * @string: A string representing the path to the file to be checked.
+ * Return: 1 if the file is executable, 0 if the file is not executable or does not exist.
+ */
 int executable_cmd(char *string)
 {
 	struct stat st;

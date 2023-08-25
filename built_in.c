be6399ecc5e3015aec0bbf7addb9525a231da734
Duncan MacLennan <6733@holbertonstudents.com>
2023-08-24 20:37:29 -0700

    function descriptions

--- built_in.c
+++ built_in.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_env - Prints the environment variables to the standard output.
+ * @array: An array of strings representing the environment variables.
+ * Return: 0 indicating successful execution.
+ */
 int print_env(char **array)
 {
 	int i;
@@ -13,8 +17,14 @@ int print_env(char **array)
 	}
 	return (0);
 }
-
-
+/**
+ * checkbuiltin - Checks if a given command is a built-in shell command
+ * @array: An array of strings representing the command and its arguments.
+ * @environ: An array of strings representing the environment variables.
+ * @str: A string that may be associated with the command, and is freed if a built-in command is found.
+ * @status: A pointer to an integer representing the exit status.
+ * Return: 1 if a built-in command is executed, 0 if the command is not a built-in command.
+ */
 int checkbuiltin(char **array, char **environ, char *str, int *status)
 {
 	if (array[0] == NULL)
@@ -35,7 +45,11 @@ int checkbuiltin(char **array, char **environ, char *str, int *status)
 	}
 	return (0);
 }
-
+/**
+ * _opendir - Opens a directory with the given name and returns a pointer to the directory stream.
+ * @name: A string representing the name of the directory to be opened.
+ * Return: Pointer to the directory stream on success. NULL if the directory could not be opened.
+ */
 DIR *_opendir(char *name)
 {
 	DIR *dp;
@@ -49,7 +63,11 @@ DIR *_opendir(char *name)
 	}
 	return (dp);
 }
-
+/**
+ * _readdir - Reads a directory entry from the directory stream pointed to by dp using the readdir function.
+ * @dp: A pointer to a directory stream.
+ * Return: Pointer to the directory entry structure if an entry is read. NULL if there are no more entries or an error occurs.
+ */
 struct dirent *_readdir(DIR *dp)
 {
 	struct dirent *ep;
